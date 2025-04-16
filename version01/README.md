80cfa90be5c6e199ece107e58e4e37f63a44b8a0
Van Appears <garethr1@yahoo.co.uk>
2025-04-16 18:55:51 +0100

    Adding version01

---
+++ version01/README.md
@@ -0,0 +1,4 @@
+# version01
+
+A super-simple script, that barely counts as a game!
+Demonstrates a gaming while loop, and a simple conditional.
---
+++ version01/game.py
@@ -0,0 +1,12 @@
+print("You are standing in a room with a closed door. How will you get out?")
+
+escaped = False
+while not escaped:
+    action = input("> ").strip().lower()
+    if action == "open door":
+        escaped = True
+    elif action != "":
+        print("I don't think that will help")
+
+print("You open the door and walk out into the sunshine. Well done!")
+print("Game over")
